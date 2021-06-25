27f1eb0d1f472ffa148bc5d36391e576b373e17e
Brian-Arruk <brian.arruk@gmail.com>
2021-06-25 00:33:48 -0300

    connection-postgres

--- src/main/java/br/com/springboot/App/SpringbootApplication.java
+++ src/main/java/br/com/springboot/App/SpringbootApplication.java
@@ -1,7 +1,8 @@
-package br.com.springboot;
+package br.com.springboot.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class SpringbootApplication {
--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.model.User;
+import br.com.springboot.repository.UserRepository;
 
 
 @RestController
@@ -20,11 +23,14 @@ public class UserController {
     
     private List<User> users = new ArrayList<>();
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public User user() {
 
         User user = new User();
-        user.setId(1L);
+        user.setId(2L);
         user.setName("Brian");
         user.setUsername("BrianArruk");
         return user;
@@ -46,8 +52,7 @@ public class UserController {
 
     @PostMapping("/")
     public User user(@RequestBody User user) {
-        users.add(user);
-        return user;
+      return this.userRepository.save(user);
     }
 
     @GetMapping("/list")
